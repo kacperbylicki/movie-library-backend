b5f17068d1482b2585fbb8db4674cd7ea6b72c8e
Kacper Bylicki <kacper@bylicki.dev>
2023-10-01 09:11:17 +0200

    feat: create basic application setup

---
+++ Startup.cs
@@ -0,0 +1,49 @@
+namespace movie_library;
+
+public class Startup
+{
+    private IConfiguration Configuration { get; }
+    
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddControllers()
+            .AddJsonOptions(
+                options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+        
+        services.AddCors(options =>
+        {
+            options.AddPolicy(name: "MovieLibraryPolicy",
+                policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                });
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseCors("MovieLibraryPolicy");
+        
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+}
